907ec38605432a21af854e1e2ef05a8cf714dc2f
pratikbbhujbal <pratik.bhujbal@aayaamai.com>
2025-07-10 17:42:35 +0530

    add ai-jobs with inngest agent-kit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 "use client"
 
 import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
 import { useTRPC } from "@/trpc/client"
 import { useMutation } from "@tanstack/react-query"
 import { toast } from "sonner"
+import { useState } from "react"
 
 const Page = () => {
+  const [value, setValue] = useState("")
   const trpc = useTRPC()
   const invoke = useMutation(
     trpc.invoke.mutationOptions({
@@ -16,9 +19,10 @@ const Page = () => {
   )
   return (
     <div className="p-4 max-w-7xl mx-auto">
+      <Input value={value} onChange={(e) => setValue(e.target.value)} />
       <Button
         disabled={invoke.isPending}
-        onClick={() => invoke.mutate({ text: "John" })}
+        onClick={() => invoke.mutate({ value })}
       >
         Invoke Background Job
       </Button>
