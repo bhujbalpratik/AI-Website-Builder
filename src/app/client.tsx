28f8929d015341e33cb2b2d166db66c6a30bb5e0
pratikbbhujbal <pratik.bhujbal@aayaamai.com>
2025-07-10 13:04:31 +0530

    add trpc setup

---
+++ src/app/client.tsx
@@ -0,0 +1,12 @@
+"use client"
+
+import { useTRPC } from "@/trpc/client"
+import { useSuspenseQuery } from "@tanstack/react-query"
+const Client = () => {
+  const trpc = useTRPC()
+  const { data } = useSuspenseQuery(
+    trpc.createAI.queryOptions({ text: "Pratik PREFETCH" })
+  )
+  return <div>{JSON.stringify(data)}</div>
+}
+export default Client
