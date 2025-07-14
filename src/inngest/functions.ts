dcd7c5df15262f5af20bd14c0ad9039eb87761bb
pratikbbhujbal <pratik.bhujbal@aayaamai.com>
2025-07-14 15:30:12 +0530

    add e2b sandbox template

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,10 +1,16 @@
+import { Sandbox } from "@e2b/code-interpreter"
 import { openai, createAgent } from "@inngest/agent-kit"
 import { inngest } from "./client"
+import { getSandbox } from "./utils"
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
-  async ({ event }) => {
+  async ({ event, step }) => {
+    const sandboxId = await step.run("get-sandbox-id", async () => {
+      const sandbox = await Sandbox.create("wibe-nextjs-test-2")
+      return sandbox.sandboxId
+    })
     const codeAgent = createAgent({
       name: "code-agent",
       system:
@@ -14,6 +20,11 @@ export const helloWorld = inngest.createFunction(
     const { output } = await codeAgent.run(
       `Write the following snippet : ${event.data.value}`
     )
-    return { output }
+    const sandboxUrl = await step.run("get-sandbox-url", async () => {
+      const sandbox = await getSandbox(sandboxId)
+      const host = sandbox.getHost(3000)
+      return `https://${host}`
+    })
+    return { output, sandboxUrl }
   }
 )
