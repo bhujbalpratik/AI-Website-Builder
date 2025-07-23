58e642c2e215d50c632481fa072cac328384a41f
pratikbbhujbal <pratik.bhujbal@aayaamai.com>
2025-07-23 23:58:41 +0530

    add billing and usage of per website

--- src/modules/home/ui/components/project-form.tsx
+++ src/modules/home/ui/components/project-form.tsx
@@ -35,14 +35,16 @@ export const ProjectForm = () => {
       onSuccess: (data) => {
         queryClient.invalidateQueries(trpc.projects.getMany.queryOptions())
         router.push(`/projects/${data.id}`)
-        // TODO: Invalidate usage stats
+        queryClient.invalidateQueries(trpc.usage.status.queryOptions())
       },
       onError: (error) => {
         toast.error(error.message)
         if (error.data?.code === "UNAUTHORIZED") {
           router.push("/sign-in")
         }
-        // TODO: Redirect to billing page if specific error
+        if (error.data?.code === "TOO_MANY_REQUESTS") {
+          router.push("/pricing")
+        }
       },
     })
   )
